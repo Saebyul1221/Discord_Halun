20ff9e0263ce77ee5c7f1b2b4c2c7230c1d76c84
saebyul1221 <dw696969@naver.com>
2022-03-01 05:23:25 +0900

    🔥사용하지 않는 테이블 제거

--- setup.sql
+++ setup.sql
@@ -25,23 +25,9 @@ CREATE TABLE authenticationlogs (
     PRIMARY KEY(`index`, `userID`)
 );
 
-/* 채팅 통계 테이블 */
-DROP TABLE IF EXISTS `chatsta`;
-CREATE TABLE chatsta (
-    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
-    `statistics` VARCHAR(255) NOT NULL DEFAULT "{}"
-);
-
 /* 입장 메시지 테이블 */
 DROP TABLE IF EXISTS `entry`;
 CREATE TABLE `entry` (
     `join` VARCHAR(255) NOT NULL DEFAULT "{user} 님께서 {guild} 에 오셨습니다. 환영해요! 현재 인원 수: **{count}**명",
     `quit` VARCHAR(255) NOT NULL DEFAULT "{user} 님께서 퇴장하셨습니다.. 현재 인원 수: **{count}**명"
-);
-
-DROP TABLE IF EXISTS `warn`;
-CREATE TABLE `warn` (
-    `userID` VARCHAR(255) NOT NULL PRIMARY KEY,
-    `count` INT NOT NULL DEFAULT 0,
-    `reason` VARCHAR(255) NOT NULL DEFAULT "[]"
 );
